527b9dc1158f45ea5d9fd1a1a34919d1ea4b6a76
Jordan Downey <jordanmdowney13@gmail.com>
2023-03-06 08:34:53 -0500

    Draft 2 - branding, move buttons/totals, add links

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 const PORT = 8080;
 
 http.createServer(function (request, response) {
-    fs.readFile('./index4.html', function (err, html) {
+    fs.readFile('./index_final.html', function (err, html) {
         if (err) throw err;
         response.writeHeader(200, { "Content-Type": "text/html" });
         response.write(html);
